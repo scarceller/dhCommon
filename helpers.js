ab74b8d8eefb40b5643abaa1a91c719f62417063
vbhadrir <vbhadrir@us.ibm.com>
2016-11-30 10:41:30 -0500

    Update helpers.js
    Changed the DB URL for Service Fabric

--- helpers.js
+++ helpers.js
@@ -17,7 +17,7 @@ var _mongoClient       = require('mongodb').MongoClient;
 //------------------------------------------------------------------------------ 
 // define mongoDB related information
 // define the DB connection url, check for env var 'mongourl'
-var _mongoURL          = process.env.mongourl || "mongodb://169.45.196.58:27017/dhOpenShift";
+var _mongoURL          = process.env.mongourl || "mongodb://169.45.196.58:27017/dhMSServiceFabric";
 // define the collection names within the DB
 var _cnameCounter      = "dhCounter";       // name of the counter collection.
 var _cnameClient       = "dhClient";        // name of the client collection.
